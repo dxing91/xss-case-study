b881a218308bd7b76237c4e34fdc266e240fc30e
dxing <domett.xing@gmail.com>
2016-04-20 14:31:32 +1000

    port

--- app.js
+++ app.js
@@ -126,7 +126,9 @@ app.post('/user-update', parseUrlencoded, function(request, response) {
   response.sendStatus(201);
 });
 
-//port 3000
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+//port
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
 });
